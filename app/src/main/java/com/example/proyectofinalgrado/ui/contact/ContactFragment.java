2ca8ff7c6f10215cd96e61c1a6f20b59a85ca10f
Alvaro <alvarosanchezhdez98@gmail.com>
2021-05-07 12:49:42 +0200

    Contact class and icons
    Programmed contact class and added new Icons to side nav bar

--- app/src/main/java/com/example/proyectofinalgrado/ui/contact/ContactFragment.java
+++ app/src/main/java/com/example/proyectofinalgrado/ui/contact/ContactFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,9 +16,14 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.proyectofinalgrado.R;
 
-public class ContactFragment extends Fragment {
+public class ContactFragment extends Fragment implements View.OnClickListener {
 
     private ContactViewModel contactViewModel;
+
+    EditText editTextPersonFullName;
+    EditText editTextEmailAddress;
+    EditText editTextDescription;
+    Button buttonSubmit;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +31,24 @@ public class ContactFragment extends Fragment {
                 ViewModelProviders.of(this).get(ContactViewModel.class);
         View root = inflater.inflate(R.layout.fragment_contact, container, false);
 
+        editTextPersonFullName = root.findViewById(R.id.editTextTextPersonFulName);
+        editTextEmailAddress = root.findViewById(R.id.editTextTextEmailAddress);
+        editTextDescription = root.findViewById(R.id.editTextDescription);
+        buttonSubmit = root.findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(this);
 
         return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+        String userFullName = editTextPersonFullName.getText().toString();
+        String userEmailAddress = editTextEmailAddress.getText().toString();
+        String userProblemDescription = editTextDescription.getText().toString();
+
+        if(!userFullName.isEmpty() && ! userEmailAddress.isEmpty() && !userProblemDescription.isEmpty()){
+
+        }
     }
 }
